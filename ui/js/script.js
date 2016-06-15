88e17531cb345a4a05976cb78270c7232e3da735
Teemo Tebest <teemo.tebest@gmail.com>
2016-06-15 12:55:12 +0300

    Started to implemented calculating assets.

--- ui/js/script.js
+++ ui/js/script.js
@@ -41,14 +41,32 @@
         complete: function () {
           if (parseInt(element.text()) !== stop) {
             element.text(stop);
-            element.text(element.text().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, '$1 '));
+            element.text(element.text().replace(/(\d)(?=(\d\d\d) + (?!\d))/g, '$1 '));
           }
         }
       });
     },
-    calculateAsset: function () {
-
-      return yleApp.getRandomInt(50, 150); 
+    getCurrentPrice: function () {
+      $('.price_value', esivis).text(yleApp.getRandomInt(50, 150));
+      return yleApp.getRandomInt(50, 150);
+    },
+    calculateAsset: function (action) {
+      // First action.
+      if (yleApp.var.lastPrice === false) {
+        yleApp.var.lastPrice = yleApp.getCurrentPrice();
+      }
+      else {
+        if (action === 'sell') {
+          yleApp.var.asset = (yleApp.var.lastPrice / yleApp.getCurrentPrice()) * yleApp.var.asset;
+        }
+        else if (action === 'buy') {
+          yleApp.var.asset = (yleApp.getCurrentPrice() / yleApp.var.lastPrice) * yleApp.var.asset;
+        }
+        return parseInt(yleApp.var.asset);
+      }
+    },
+    initAsset: function () {
+      $('.result_value', esivis).text(yleApp.var.asset);
     },
     initEvents: function () {
       $(window).resize(function () {
@@ -61,20 +79,23 @@
       });
       // Sell event.
       esivis.on('click', '.control.buy', function (event) {
-        yleApp.initNumbers($('.result_value', esivis), yleApp.calculateAsset(), 1000, 'swing');
+        yleApp.initNumbers($('.result_value', esivis), yleApp.calculateAsset('buy'), 1000, 'swing');
         event.preventDefault();
       });
       // Buy event.
       esivis.on('click', '.control.sell', function (event) {
-        yleApp.initNumbers($('.result_value', esivis), yleApp.calculateAsset(), 1000, 'swing');
+        yleApp.initNumbers($('.result_value', esivis), yleApp.calculateAsset('sell'), 1000, 'swing');
         event.preventDefault();
       });
     },
     init: function () {
       yleApp.var = {};
-      yleApp.var.asset = 1;
+      yleApp.var.asset = 100;
+      yleApp.var.lastPrice = false;
       yleApp.initEvents();
       yleApp.fixHeights();
+      yleApp.initAsset();
+      new Vis().init();
     }
   };
   $(document).ready(function () {
