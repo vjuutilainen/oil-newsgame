93fef797532d7c4dc579650692fd2517e3b982ca
Ville Juutilainen <vjuutilainen@gmail.com>
2016-06-15 16:04:14 +0200

    kaikenlaista

--- vis/script.js
+++ vis/script.js
@@ -3,19 +3,34 @@
 const formatDate = d3.time.format('%YM%m');
 
 const events = {
-  '1960M01': { title: 'Something here' },
-  '1965M01': { title: 'Something here' },
-  '1970M01': { title: 'Something here' },
-  '1975M01': { title: 'Something here' },
-  '1980M01': { title: 'Something here' },
-  '1985M01': { title: 'Something here' },
-  '1990M01': { title: 'Something here' },
-  '1995M01': { title: 'Something here' },
-  '2000M01': { title: 'Something here' },
-  '2005M01': { title: 'Something here' },
-  '2010M01': { title: 'Something here' },
-  '2015M01': { title: 'Something here' }
+  '1960M01': { title: 'Something here 1' },
+  '1965M01': { title: 'Something here 2' },
+  '1970M01': { title: 'Something here 3' },
+  '1975M01': { title: 'Something here 4' },
+  '1980M01': { title: 'Something here 5' },
+  '1985M01': { title: 'Something here 6' },
+  '1990M01': { title: 'Something here 7' },
+  '1995M01': { title: 'Something here 8' },
+  '2000M01': { title: 'Something here 9' },
+  '2005M01': { title: 'Something here 10' },
+  '2010M01': { title: 'Something here 11' },
+  '2015M01': { title: 'Something here 12' }
 };
+
+const sampleTransactions = [
+  { date: formatDate.parse('1960M01'), type: 'BUY', price: 1.63 },
+  { date: formatDate.parse('1965M01'), type: 'BUY', price: 2 },
+  { date: formatDate.parse('1970M01'), type: 'BUY', price: 3 },
+  { date: formatDate.parse('1975M01'), type: 'BUY', price: 4 },
+  { date: formatDate.parse('1980M01'), type: 'BUY', price: 5 },
+  { date: formatDate.parse('1985M01'), type: 'BUY', price: 6 },
+  { date: formatDate.parse('1990M01'), type: 'BUY', price: 7 },
+  { date: formatDate.parse('1995M01'), type: 'BUY', price: 8 },
+  { date: formatDate.parse('2000M01'), type: 'BUY', price: 9 },
+  { date: formatDate.parse('2005M01'), type: 'BUY', price: 10 },
+  { date: formatDate.parse('2010M01'), type: 'BUY', price: 11 },
+  { date: formatDate.parse('2015M01'), type: 'BUY', price: 12 }
+];
 
 class Vis {
   
@@ -27,21 +42,31 @@ class Vis {
     this.info = this.container.append('div').attr('class', 'info');
     this.svg = this.container.append('svg');
     this.content = this.svg.append('g').attr('class', 'content');
-    
     this.scaleX = d3.time.scale();
     this.scaleY = d3.scale.linear();
-    this.axisX = d3.svg.axis().orient('bottom').ticks(10);
+    this.axisX = d3.svg.axis().orient('top').ticks(10);
     this.axisY = d3.svg.axis().orient('right').ticks(10);
     this.axisGroupX = this.content.append('g').attr('class', 'axis-x');
     this.axisGroupY = this.content.append('g').attr('class', 'axis-y');
-    this.lineGenerator = d3.svg.line();
+    this.lineGenerator = d3.svg.line().interpolate('basis');
     this.line = null;
     this.eventMarkers = null;
+
+    this.beginTime = formatDate.parse('1960M01');
+    this.currentTime = formatDate.parse('1960M01');
+    this.finalTime = formatDate.parse('2016M05');
+
+    this.currentPrice = null;
+    this.transactionData = [];
 
     window.onresize = () => {
       this.resizeGraph();
       this.updateGraph();
-      this.updateMarkers();
+      this.updateInfoMarker();
+      this.updatetransactionMarkers();
+      
+
+      //this.updateMarkers();
     };
 
   }
@@ -50,6 +75,12 @@ class Vis {
     d3.csv('vis/data/oil.csv', this.transformData, (err, data) => {
       if(err) throw err;
       this.priceData = data;
+      // make sample transactions !!!
+      this.transactionData = data.filter(d => Math.random() > 0.98).map(d => {
+        d.type = Math.random() > 0.5 ? 'BUY' : 'SELL';
+        return d;
+      }); 
+      this.currentPrice = data[0].price;
       cb(data);
     });
   }
@@ -57,34 +88,44 @@ class Vis {
   initGraph(data) {
     this.line = this.content.append('path')
                             .attr('fill', 'none')
-                            .attr('stroke', 'black')
-                            .attr('class', 'line').datum(data);
+                            .attr('stroke-width', 4)
+                            .attr('stroke', 'white')
+                            .attr('class', 'line');
   }
 
-  initMarkers() {
-
-    this.eventMarkers = this.content
-                      .selectAll('.eventmarker')
-                      .data(this.priceData.filter(d => d.event))
-                      .enter()
-                      .append('circle')
-                      .attr('r', '5')
-                      .attr('fill', 'red');
+  initTransactionMarkers() {
+    this.transactionMarkers = this.content.selectAll('.transactionmarker');
   }
 
-  updateMarkers() {
-    this.eventMarkers
+  updatetransactionMarkers() {
+    let join = this.transactionMarkers.data(this.transactionData);
+
+    join.exit().remove();
+
+    join.enter()
+        .append('circle')
+        .attr('r', '5')
+        .attr('fill', 'red');
+
+    join
         .attr('cx', (d) => this.scaleX(d.date))
         .attr('cy', (d) => this.scaleY(d.price));
+
+    this.transactionMarkers = join;
+
   }
 
-  init() {
+  init(cb) {
     this.loadData((data) => {
-      this.initGraph(data);
-      this.initMarkers();
+      this.initGraph();
+      this.initTransactionMarkers();
+      
+      this.initInfoMarker();
+      this.initInfo();
       this.resizeGraph();
       this.updateGraph();
-      this.updateMarkers();
+      this.updatetransactionMarkers();
+      if(cb) cb(this);
     });
   }
 
@@ -95,9 +136,127 @@ class Vis {
     this.svg.attr('width', this.width).attr('height', this.height);
   }
 
+  getDataCrop(data) {
+
+    let endTime = new Date(this.currentTime);
+    endTime.setFullYear(endTime.getFullYear() + 10);
+    
+    return data.filter(d => {
+      return d.date >= this.currentTime && d.date <= endTime;
+    });
+
+    //return data;
+  
+  }
+
+  updateInfoMarker() {
+    this.infoMarker.attr({
+      width: 100,
+      height: this.height,
+      x: this.width - 100,
+      y: 0,
+    });
+  }
+
+  initInfoMarker() {
+    this.infoMarker = this.content.append('rect').attr({
+      width: 100,
+      height: this.height,
+      x: this.width - 100,
+      y: 0,
+      fill: '#000', 
+      opacity: 0.5
+    });
+  }
+
+  initInfo() {
+
+    let html = '<h3>Year ' + this.currentTime.getFullYear() + '</h3>' +
+               '<h3>Price ' + this.currentPrice + '</h3>';
+
+    this.info.html(html);
+
+  }
+
+  updateInfo() {
+    //let identifier = this.currentTime.getFullYear() + 'M' + (this.currentTime.getMonth() + 1);
+    //console.log(identifier);
+    //if(events[identifier]) {
+      let html = 
+                 '<h3>Year ' + this.currentTime.getFullYear() + ' M' + (this.currentTime.getMonth() + 1)  + '</h3>' +
+                 '<h3>Price ' + this.currentPrice + '</h3>';
+      this.info.html(html);
+    //}
+    //else {
+      //info.attr('opacity')
+    //}
+    
+  }
+
+  update() {
+
+    let newTime = new Date(this.currentTime);
+    newTime.setMonth(newTime.getMonth() + 1);
+    this.currentTime = newTime;
+    if(this.currentTime >= this.finalTime) {
+      this.playing = false;
+      this.currentTime = this.finalTime;
+    }
+
+    this.updateGraph();
+    this.updateInfo();
+    this.updatetransactionMarkers();
+
+  }
+
+  play() {
+
+    this.playing = true;
+    
+    let tick = () => {
+      if(this.playing && this.currentTime !== this.finalTime) {
+       requestAnimationFrame(tick); 
+       this.update();
+      }
+    };
+
+    requestAnimationFrame(tick);
+  }
+
+  buy() {
+    this.addTransactionMarker('BUY');
+  }
+
+  sell() {
+    this.addTransactionMarker('SELL');
+  }
+
+  addTransactionMarker(type) {
+    this.transactionData.push({type: type, date: this.currentTime, price: this.currentPrice });
+  }
+
+  stop() {
+    this.playing = false;
+  }
+
+  getCurrentPrice() {
+    return this.currentPrice;
+  }
+
+   getCurrentYear() {
+    return this.currentTime;
+  }
+
   updateGraph() {
-    this.scaleX.domain(d3.extent(this.priceData, (d) => d.date ));
-    this.scaleY.domain(d3.extent(this.priceData, (d) => d.price ));
+
+    let data = this.getDataCrop(this.priceData);
+    this.currentPrice = data[0].price;
+    
+    //console.log(data);
+
+    this.scaleX.domain(d3.extent(data, (d) => d.date ));
+    this.scaleY.domain(d3.extent(data, (d) => d.price ));
+
     this.scaleX.range([0, this.width]);
     this.scaleY.range([this.height, 0]);
     this.axisX.scale(this.scaleX);
@@ -110,19 +269,28 @@ class Vis {
                       .y((d) => { return this.scaleY(d.price); });
 
 
-    this.line.attr('d', this.lineGenerator);
+    this.line.datum(data);
+
+    let timeTick = new Date(this.currentTime);
+    timeTick.setMonth(timeTick.getMonth() + 1);
+
+    let timeOffset = new Date(this.currentTime);
+    timeOffset.setYear(timeOffset.getFullYear() + 10);
+
+    this.line
+        .attr('d', this.lineGenerator);
+
+    // this.content
+    //     .attr('transform', null)
+    //     .attr('transform', 'translate(' + 0.01 + ')');
 
   }
 
   transformData(d) {
     d.date = formatDate.parse(d.time);
     d.price = +d.crude_petro_usd_bbl;
-    d.event = events[d.time] ? events[d.time] : false; 
     return d;
   }
 
 }
 
-window.onload = () => {
-  // new Vis().init();
-};
