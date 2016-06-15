30df3e39335e10b2a0008f3ded038287dceff3f5
Ville Juutilainen <vjuutilainen@gmail.com>
2016-06-15 10:38:45 +0200

    vis preparations

--- vis/script.js
+++ vis/script.js
@@ -1,26 +1,6 @@
+'use strict';
 
-var App = function() {
-  this.oildata = null;
-};
+class Vis {
 
+}
 
-App.prototype = {
-  init: function() {
-    this.loadData();
-    this.initGraph();
-  },
-  initGraph: function() {
-
-  },
-  loadData() {
-    d3.csv('data/oil.csv', function(data) {
-      this.oildata = data;
-    }.bind(this));
-  }
-};
-
-
-window.onload = function() {
-  var app = new App();
-  app.init();
-};
